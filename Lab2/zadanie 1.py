8decd65febd785ab65ba8ad281430edfb1b8b54b
D41342 <Dmytrostars@ukr.net>
2022-10-20 15:12:35 +0200

    Rozwiązania zadań z Lab 2 (zad 1-3)

---
+++ Lab2/zadanie 1.py
@@ -0,0 +1,15 @@
+#Dla osób poniżej 4 roku życia wstęp jest bezpłatny.
+#• Dla osób w wieku od 4 do 18 lat bilet kosztuje 10zł.
+#• Dla osób powyżej 18 roku życia bilet kosztuje 20zł.
+#Przykład: Wprowadź wiek klienta: 5
+#Cena biletu: 10zł
+
+x = int(input ("Podai wiek"))
+if x < 4:
+    cena = 0
+elif x <= 18:
+    cena = 10
+else :
+    cena = 20
+
+print (f"Cena biletu: {cena}zł")
